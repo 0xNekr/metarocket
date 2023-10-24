63c60d659464fb9919bfcc144d2d09bd76a12990
0xNekr <0xNekr.dev@gmail.com>
2023-10-24 19:07:20 +0200

    feat: Add rocket structure and enum of states

--- packages/solidity/contracts/MetaRocket.sol
+++ packages/solidity/contracts/MetaRocket.sol
@@ -5,6 +5,21 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MetaRocket is ERC721 {
 
+    enum RocketState { NOT_LAUNCHED, IN_FLIGHT, OUT_OF_GAS, EXPLODED, BLACK_HOLE }
+
+    struct rocket {
+        uint256 tokenId;
+        string name;
+        string description;
+        uint64 gasLeft;
+        uint64 jumpCount;
+        uint64 creationTimestamp;
+        uint64 lastJumpTimestamp;
+        RocketState state;
+        bool gasFree;
+        bool indestructible;
+    }
+
     constructor() ERC721("MetaRocket", "MTR") {}
 
 }
